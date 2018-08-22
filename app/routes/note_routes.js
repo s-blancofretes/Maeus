ab5f8eceda796578e9aedae3582c764f9883954d
s-blancofretes <s.blancofretes@gmail.com>
2018-08-21 23:26:22 -0300

    Created functions directory, Implemented webhook route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,30 +1,18 @@
 var ObjectID = require('mongodb').ObjectID;
+var utils = require('../functions/utils')
+
 module.exports = function(app, db) {
-  //READ NOTE  
-  app.get('/notes/:id', (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    db.collection('notes').findOne(details, (err, item) => {
-      if (err) {
-        res.send({'error':'An error has occurred'});
-      } else {
-        res.send(item);
-      } 
-    });
-  });
-//CREATE NOTE
-app.post('/notes', (req, res) => {
-    const note = { text: req.body.body, title: req.body.title };
-    db.collection('notes').insert(note, (err, result) => {
-      if (err) { 
-        res.send({ 'error': 'An error has occurred' }); 
-      } else {
-        res.send(result.ops[0]);
+//WEBHOOK
+app.post('/webhook', (req,res) => {
+  var reqJson = req.body;
+    for (var i = 0; i < reqJson.messages.length; i++) {
+      var message = reqJson.messages[i];
+      if(message.body=="Clave"){
+        //Aca va la persistencia a la DB
+        console.log("Phone: " + utils.splitPhone(message.author));
+        console.log("Timestamp: " + message.time);
       }
-    });
-  });
-//WEBHOOK TEST
-app.post('/webhook', (req,res)=>{
-  
+  }
+res.send("200 OK")
 });
 };
