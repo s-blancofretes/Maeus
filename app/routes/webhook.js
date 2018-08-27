e51200f9cd478197037083b60b807de8e08bf5f9
s-blancofretes <s.blancofretes@gmail.com>
2018-08-26 23:04:09 -0300

    Created whatsapp.js containing sendFile and sendMessage functions

--- app/routes/webhook.js
+++ app/routes/webhook.js
@@ -1,5 +1,6 @@
 var ObjectID = require('mongodb').ObjectID;
 var utils = require('../functions/utils')
+var whatsapp = require('../functions/whatsapp')
 
 module.exports = function(app, db) {
 //WEBHOOK
@@ -8,6 +9,8 @@ app.post('/webhook', (req,res) => {
     for (var i = 0; i < reqJson.messages.length; i++) {
       var message = reqJson.messages[i];
       if(message.body=="Clave"){
+        //whatsapp.sendMessage(message.author,"Respuesta Superautomatica")
+        whatsapp.sendFile(message.author,'Currywurst','https://ais.kochbar.de/kbrezept/515109_897054/620x465/currywurst-gulasch-mit-backkartoffeln-rezept-bild-nr-4945.jpg')
         //Aca va la persistencia a la DB
         console.log("Phone: " + utils.splitPhone(message.author));
         console.log("Timestamp: " + message.time);
