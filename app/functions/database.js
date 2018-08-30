ea009f8f9cb704a339f253783796fb03ef13aa63
gascione <gaglausiuss@gmail.com>
2018-08-30 19:52:28 -0300

    DB module fixed

--- app/functions/database.js
+++ app/functions/database.js
@@ -1,11 +1,7 @@
-var db= require('../../config/db');
-const MongoClient    = require('mongodb').MongoClient;
-
 module.exports = {
-    dbUser : function(phone, idUser, idStory){
+    dbCreateNewUser : function(db, phone, idUser, idStory){
         const user = { cellphone: phone, idContact: idUser, story: idStory};
         db.collection('usuarios').insert(user, (err, result) => {
-          
           if (err) { 
             //res.send({ 'error': 'An error has occurred' }); 
           } else {
--- app/routes/webhook.js
+++ app/routes/webhook.js
@@ -1,11 +1,10 @@
-var ObjectID = require('mongodb').ObjectID;
 const bodyParser= require('body-parser');
 var utils = require('../functions/utils')
 var whatsapp = require('../functions/whatsapp')
-var db= require('../../config/db');
+
 
 //archivo para los insert de la base de datos
-//var dataBase = require('../functions/database') 
+var database = require('../functions/database') 
 
 module.exports = function(app, db) {
 //WEBHOOK
@@ -18,23 +17,13 @@ app.post('/webhook', (req,res) => {
         //whatsapp.sendMessage(message.author,"Respuesta Superautomatica")
         whatsapp.sendFile(message.author,'Currywurst','https://ais.kochbar.de/kbrezept/515109_897054/620x465/currywurst-gulasch-mit-backkartoffeln-rezept-bild-nr-4945.jpg')
         var cellphone = utils.splitPhone(message.author);
-        //Aca va la persistencia a la DB
-
-        const user = { cellphone: cellphone, idContact: message.author, story: 1};
-        db.collection('usuarios').insert(user, (err, result) => {
-          //ver como hacer esta verificacion sin que se manden dos respuestas
-          if (err) { 
-            //res.send({ 'error': 'An error has occurred' }); 
-          } else {
-            //res.send(result.ops[0]);
-          }
-        });
-        //dataBase.dbUser(cellphone, message.author, 1);
+        //Aca va la persistencia a la DB 
+        database.dbCreateNewUser(db, cellphone, message.author, 1);
         
         console.log("Phone: " + utils.splitPhone(message.author));
         console.log("Timestamp: " + message.time);
+        
       }
   }
-res.send("200 OK")
 });
 };
