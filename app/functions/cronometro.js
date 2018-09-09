fc2a143214cb3695eef03ba331f86299ac5a94dd
gascione <gaglausiuss@gmail.com>
2018-09-08 21:10:19 -0300

    Checking objects in  database with comments

--- app/functions/cronometro.js
+++ app/functions/cronometro.js
@@ -8,19 +8,22 @@
     }
 
 };*/
-
+//Funcion que chequea si existen contactos que esten en la historia 1 y hayan recibido mensaje 1
 module.exports = {
     checkMsj : function(db, wapp, idStory, numMsj){
+        //query que contiene los datos que queremos averiguar
         var query = {story: idStory, msjNumb: numMsj};
-        var contactos = [];
+        //accedemos a la base de datos 
         db.collection('usuarios').find(query).toArray(function(err, result) {
         if (err) throw err;
         else{
             console.log(result);
+            //for que recorre el array de contactos que cumplen con esas condiciones
             for (var i = 0; i < result.length; i++) {
                 var contacto = result[i];
                 var idContact = contacto.idContact;
                 console.log(idContact)
+                //funcion del archivo whatapp que manda un mensaje
                 wapp.sendMessage(idContact, 'hola');
                 
     
