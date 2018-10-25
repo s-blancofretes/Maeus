0476e52662805ec91f8c6e5e8e0d7ec2de9400e5
germandrq <32778117+germandrq@users.noreply.github.com>
2018-10-25 15:57:51 -0300

    update con función de seleccionar historia

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb-promise').MongoClient;
 const bodyParser = require('body-parser');
 var db = require('./config/db');
 
 const app = express();
 
-const port = process.env.PORT || 8000;
+const port = 8000;
 
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json())
 
-MongoClient.connect(db.url, (err, database) => {
+MongoClient.connect(db.url).then((err, database) => {
     if (err) return console.log(err)
     db = database.db("progeus18")
     require('./app/routes')(app, db);
