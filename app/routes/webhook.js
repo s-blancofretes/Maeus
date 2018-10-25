14570ada455b9eaa690ed8e7991b0a87d13ebc3c
germandrq <32778117+germandrq@users.noreply.github.com>
2018-10-25 15:58:50 -0300

    Add files via upload

--- app/routes/webhook.js
+++ app/routes/webhook.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 var utils = require('../functions/utils')
 var whatsapp = require('../functions/whatsapp')
+const experiencias = require('../experiencias');
 
 //archivo para los insert de la base de datos
 var database = require('../functions/database')
@@ -15,41 +16,32 @@ var verificador = require('../functions/checkingMsj')
 
 
 
-module.exports = function (app, db) {
-
-    app.get('/health', (req, res) => {
-        res.json({
-            ok: true,
-        });
-
-    });
-
+module.exports = function(app, db) {
     //WEBHOOK
-    app.post('/webhook', (req, res) => {
+    app.post('/webhook', async(req, res) => {
         var reqJson = req.body;
         for (var i = 0; i < reqJson.messages.length; i++) {
             var message = reqJson.messages[i];
 
             if (message.body == "Clave") {
-                //whatsapp.sendMessage(message.author,"Respuesta Superautomatica")
+
                 whatsapp.sendFile(message.author, 'Currywurst', 'https://ais.kochbar.de/kbrezept/515109_897054/620x465/currywurst-gulasch-mit-backkartoffeln-rezept-bild-nr-4945.jpg')
+
                 var cellphone = utils.splitPhone(message.author);
-                //Aca va la persistencia a la DB 
-                //guardamos en esta variable el resultado si dio error o no 
-                var respuesta = database.dbCreateNewUser(db, cellphone, message.author, 1, 1, message.time);
+                var author = utils.splitPhone(message.author);
+                var time = message.time;
+                var experienciaActual = experiencias.obtenerExperienciaSegunHoraDeInicio();
 
-                //Seteo del Cron 
-                //schedulerCrono.startScheduler(cron);
-
+                if (experienciaActual !== null) {
+                    await database.dbCreateNewExperience(db, { numeroMovile: cellphone, idExperiencia: experienciaActual.id });
+                }
 
                 console.log("Phone: " + utils.splitPhone(message.author));
                 console.log("Timestamp: " + message.time);
-                //mandamos las variables db y whatsapp para poder acceder a sus funciones desde el otro script
-                //funcion cronometro que va a verificar cada un intervalo 
-                //de tiempo quien esta en la historia 1 y mensaje 1
-                startExperience.cronometro(db, whatsapp, verificador, 1);
 
-                res.send(respuesta)
+                res.json({
+                    ok: true
+                })
             }
         }
     });
