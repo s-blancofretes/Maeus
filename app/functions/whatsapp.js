6e120ec3d4ec175f53d0a3553672c1694fd833f8
German <germandrq@gmail.com>
2018-11-05 03:52:24 -0300

    agrego nodemailer

--- app/functions/whatsapp.js
+++ app/functions/whatsapp.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const bodyParser = require('body-parser');
 const querystring = require('querystring')
+var email = require('../functions/email');
 const url = 'https://eu10.chat-api.com/instance8578';
 var token = { token: 'eu8htn7gz3no08ed' };
 
@@ -61,10 +62,13 @@ module.exports = {
             //console.log(res.statusCode);
             console.log(res.body.totalMessages); //no se porque me da un undefine
             var totalMsg = res.body.totalMessages;
+            let mailOptions2 = email.mailOptions;
+            let transporter2 = email.transporter;
             if (totalMsg > 5) {
                 console.log("ALERTA_5");
             } else if (totalMsg > 1) {
                 console.log("ALERTA_1");
+                //email.enviar(); invocar enviador de emails con parametro totalmsg
             }
         });
     },
