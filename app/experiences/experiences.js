8880fd7c6787675a63bc547c0bfebc6deb84dbd0
gascione <gaglausiuss@gmail.com>
2018-11-13 10:28:17 -0300

    Olivia's chat updated

--- app/experiences/experiences.js
+++ app/experiences/experiences.js
@@ -21,7 +21,7 @@ module.exports = {
     "2": {
         id: 2,
         name: "Olivia",
-        totalMsg: 16,
+        totalMsg: 39,
         startTime: "18:00",
         endTime: "23:00",
         duration: 30,
@@ -33,108 +33,269 @@ module.exports = {
                 isFile: true
             },
             {
-                text: "¡Nutriéndome para la noche!",
+                text: "Un videito para que no me extrañes",
+                fileName: null,
+                url: null,
+                msgTime: 5,
+                isFile: false
+            },
+            {
+                text: "Una lastima que no puedas estar 😢😢",
+                fileName: null,
+                url: null,
+                msgTime: 10,
+                isFile: false
+            },
+            {
+                text: "Y vos? en que andas?",
+                fileName: null,
+                url: null,
+                msgTime: 15,
+                isFile: false
+            },
+            {
+                text: "🙏🙏🙏 no llegues tarde!!",
+                fileName: null,
+                url: null,
+                msgTime: 20,
+                isFile: false
+            },          
+            {
+                text: "Nutriéndome para la noche 😋 🍴",
                 fileName: "img_2301.jpg",
                 url: "/olivia/O_002.jpg",
-                msgTime: 420,
+                msgTime: 300,
                 isFile: true
             },
             {
-                text: null,
+                text: "Te escribo cuando termine de comer",
+                fileName: null,
+                url: null,
+                msgTime: 305,
+                isFile: false
+            },
+            {
+                text: "Ahora en un ratito entro a bañarme 🚿 🚿 ",
+                fileName: null,
+                url: null,
+                msgTime: 720,
+                isFile: false
+            },
+            {
+                text: "Que nerviosss!!!!",
+                fileName: null,
+                url: null,
+                msgTime: 730,
+                isFile: false
+            },
+            {
+                text: "Mira la canasta que me prepararon 🔝🔝🔝" ,
                 fileName: "img_2302.jpg",
                 url: "/olivia/O_003.jpg",
-                msgTime: 1020,
+                msgTime: 780,
                 isFile: true
+            },
+            {
+                text: "La verdad, unos genios! ",
+                fileName: null,
+                url: null,
+                msgTime: 785,
+                isFile: false
+            },
+            {
+                text: "Jajaja no sabes lo que me trajo Belu",
+                fileName: null,
+                url: null,
+                msgTime: 1020,
+                isFile: false
+            },
+            {
+                text: "😂😂🙈🙈",
+                fileName: null,
+                url: null,
+                msgTime: 1025,
+                isFile: false
+            },
+            {
+                text: "Ya te muestro",
+                fileName: null,
+                url: null,
+                msgTime: 1030,
+                isFile: false
             },
             {
                 text: null,
                 fileName: "vid_1502.mp4",
                 url: "/olivia/O_004.mp4",
-                msgTime: 1800,
+                msgTime: 1200,
                 isFile: true
+            },
+            {
+                text: "Increíble como me quedó la piel",
+                fileName: null,
+                url: null,
+                msgTime: 1440,
+                isFile: false
+            },
+            {
+                text: "En breves empiezo con el maquillaje ",
+                fileName: null,
+                url: null,
+                msgTime: 1450,
+                isFile: false
+            },
+            {
+                text: "😱😱",
+                fileName: null,
+                url: null,
+                msgTime: 1453,
+                isFile: false
             },
             {
                 text: null,
                 fileName: "vid_1503.mp4",
                 url: "/olivia/O_005.mp4",
-                msgTime: 2400,
+                msgTime: 2100,
                 isFile: true
+            },
+            {
+                text: " Por subir este video a mi Instagram me hizo descuento jajaj ",
+                fileName: null,
+                url: null,
+                msgTime: 2110,
+                isFile: false
+            },
+            {
+                text: "Yo encantada 🌟🌟🌟",
+                fileName: null,
+                url: null,
+                msgTime: 2115,
+                isFile: false
             },
             {
                 text: null,
                 fileName: "vid_1504.mp4",
                 url: "/olivia/O_006.mp4",
-                msgTime: 3600,
+                msgTime: 3000,
                 isFile: true
+            },
+            {
+                text: "Y?!?!?!?!?!?",
+                fileName: null,
+                url: null,
+                msgTime: 3010,
+                isFile: false
+            },
+            {
+                text: "Que te pareció?!!??!",
+                fileName: null,
+                url: null,
+                msgTime: 3015,
+                isFile: false
+            },
+            {
+                text: "NO LO PUEDO CREER",
+                fileName: null,
+                url: null,
+                msgTime: 3020,
+                isFile: false
+            },
+            {
+                text: "YA LLEGA",
+                fileName: null,
+                url: null,
+                msgTime: 3025,
+                isFile: false
             },
             {
                 text: "Próximo encuentro: iglesia 🔜 + 👰 + 💒",
                 fileName: "img_2303.jpg",
                 url: "/olivia/O_007.jpg",
-                msgTime: 3780,
+                msgTime: 3600,
                 isFile: true
+            },
+            {
+                text: "Bueno ya me confirmaron el remise",
+                fileName: null,
+                url: null,
+                msgTime: 3780,
+                isFile: false
+            },
+            {
+                text: "Un embole esperar sola 😁😁",
+                fileName: null,
+                url: null,
+                msgTime: 3787,
+                isFile: false
+            },
+            {
+                text: "Por suerte te tengo a vos",
+                fileName: null,
+                url: null,
+                msgTime: 3795,
+                isFile: false
             },
             {
                 text: "La puta madre!!",
                 fileName: null,
                 url: null,
-                msgTime: 4800,
+                msgTime: 4680,
                 isFile: false
             },
             {
                 text: "El remise ya tendría que estar acá",
                 fileName: null,
                 url: null,
-                msgTime: 4810,
+                msgTime: 4690,
                 isFile: false
             },
             {
                 text: "Nonono me muero si este tipo no aparece",
                 fileName: null,
                 url: null,
-                msgTime: 4820,
+                msgTime: 4700,
                 isFile: false
             },
             {
                 text: "O lo mato 🔪",
                 fileName: null,
                 url: null,
-                msgTime: 4825,
+                msgTime: 4705,
                 isFile: false
             },
             {
                 text: null,
                 fileName: "au_7601.mp3",
                 url: "/olivia/O_012.mp3",
-                msgTime: 5425,
+                msgTime: 4825,
                 isFile: true
             },
             {
                 text: null,
                 fileName: "au_7602.mp3",
                 url: "/olivia/O_013.mp3",
-                msgTime: 5460,
+                msgTime: 4885,
                 isFile: true
             },
             {
                 text: null,
                 fileName: "au_7603.mp3",
                 url: "/olivia/O_014.mp3",
-                msgTime: 5760,
+                msgTime: 5160,
                 isFile: true
             },
             {
                 text: "Se me llega a arruinar el vestido y mato a alguien.",
                 fileName: "img_2304.jpg",
                 url: "/olivia/O_015.jpg",
-                msgTime: 5820,
+                msgTime: 5340,
                 isFile: true
             },
             {
                 text: null,
                 fileName: "au_7604.mp3",
                 url: "/olivia/O_016.mp3",
-                msgTime: 6000,
+                msgTime: 5640,
                 isFile: true
             }
         ]
